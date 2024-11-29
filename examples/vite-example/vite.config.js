11f954dceed0c7b98ba1eab18489acb583050348
Eyüp <epsilonelittr@gmail.com>
2024-11-29 14:49:18 +0300

    Update deps

--- examples/vite-example/vite.config.js
+++ examples/vite-example/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    target: 'es2020'
-  }
-})
+    target: "es2020",
+  },
+  server: {
+    port: 8000,
+  },
+});
